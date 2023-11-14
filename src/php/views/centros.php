a27583bb4c9411919addf349eec886cc01f5a4ed
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2023-11-14 12:01:26 +0100

    sql ,index y mas

---
+++ src/php/views/centros.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>centros</title>
+</head>
+<body>
+    
+<h2>Tabla - Centro</h2>
+<button>Añadir</button>
+<table>
+
+        <tr>
+            <th></th>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+       
+</table>
+</body>
+</html>
