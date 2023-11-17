85641fb7c209c677b8147bb15a8ffa49db5a7fcf
nacedodev <121324366+nacedodev@users.noreply.github.com>
2023-11-17 16:56:52 +0100

    paths DWES

--- src/php/controllers/centros.php
+++ src/php/controllers/centros.php
@@ -1,6 +1,6 @@
 <?php
  
-require_once './models/centros.php';
+require_once '../models/centros.php';
 
 class ControladorCentros {
    
--- src/php/controllers/clases.php
+++ src/php/controllers/clases.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './models/clases.php';
+require_once '../models/clases.php';
 
 class ControladorClases{
 
