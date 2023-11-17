8be42800b537453e3d5bc4f2af98de8feceff936
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2023-11-17 18:40:20 +0100

    sql

---
+++ sql/tabla_clase.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Clase (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    etapa VARCHAR(50) NOT NULL,
+    clase VARCHAR(50) NOT NULL,
+    centro_id INT,
+    CONSTRAINT fk_nombre_centro 
+        FOREIGN KEY (centro_id) 
+        REFERENCES Centro(id) 
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
