1a57a55b2cf70e745283d2463895ab281bb5a786
Mario Fuentes <mariofuentesf16.gmail.com>
2023-11-21 09:50:38 +0100

    creacion de sql base de datos

---
+++ sql/tabla_conflicto.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Conflicto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombreconflicto VARCHAR(100) NOT NULL,
+    posx smallint NOT NULL,
+    posy smallint NOT NULL,
+    imagenconflicto smallint NOT NULL,
+    estadoconflicto VARCHAR(50)
+    nivel_id INT,
+    CONSTRAINT fk_nombre_nivel 
+        FOREIGN KEY (nivel_id) 
+        REFERENCES Nivel(id) 
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
