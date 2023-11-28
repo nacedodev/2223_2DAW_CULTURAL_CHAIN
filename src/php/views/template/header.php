18bc271cf6e0b1e6e52e8385312e228726c4ae5a
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2023-11-28 22:57:34 +0100

    arreglar menu

--- src/php/views/template/header.php
+++ src/php/views/template/header.php
@@ -8,12 +8,21 @@
     </head>
     <body>
         <nav>
-            <ul>
+            <ul id="menu">
                 <li><a href="index.php" id="alogo"><img id="logo" src="../img/iconos/logo.jpeg"></a></li>
                 <li> <a href=""><span>PERSONAJES</span><img src="../img/iconos/diseno-de-personaje.png"></a></li>
                 <li><a href=""><span>REFLEXIONES</span><img src="../img/iconos/idea.png"></a></li>
                 <li><a href="index.php?controller=centros&action=listarCentros"><span>CENTROS</span><img src="../img/iconos/colegio.png"></a></li>
                 <li><a href="index.php?controller=niveles&action=listarNiveles"><span>NIVELES</span><img src="../img/iconos/mapa-del-mundo.png"></a></li>
                 <li><a href=""><span>PUNTUACIONES</span><img src="../img/iconos/opinion-del-cliente.png"></a></li>
+                <li id="barras"> - - - 
+                    <ul id="submenu">
+                        <li><a href=""><span>PERSONAJES</span><img src="../img/iconos/diseno-de-personaje.png"></a></li>
+                        <li><a href=""><span>REFLEXIONES</span><img src="../img/iconos/idea.png"></a></li>
+                        <li><a href="index.php?controller=centros&action=listarCentros"><span>CENTROS</span><img src="../img/iconos/colegio.png"></a></li>
+                        <li><a href="index.php?controller=niveles&action=listarNiveles"><span>NIVELES</span><img src="../img/iconos/mapa-del-mundo.png"></a></li>
+                        <li><a href=""><span>PUNTUACIONES</span><img src="../img/iconos/opinion-del-cliente.png"></a></li>
+                    </ul>
+                </li>
             </ul>
         </nav>
