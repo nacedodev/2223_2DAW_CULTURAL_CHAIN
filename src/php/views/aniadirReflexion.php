e3e94dc3ac044e6ee6939a210108aa829c452f44
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2023-11-28 18:36:40 +0100

    formulario reflexiones mod

--- src/php/views/aniadirReflexion.php
+++ src/php/views/aniadirReflexion.php
@@ -1,22 +1,21 @@
 </head>
 <body>
-        <button class="boton" style="margin-left:44%" onclick="agregarFormulario()"><img src="../img/iconos/anadir.png" alt="+"></button>
-        <button class="boton" style="margin-left:5%"><img src="../img/iconos/x.png" alt="-"></button>
+        <button class="boton" style="margin-left:35%;  width:30%; height: 65px " onclick="agregarFormulario()"><img src="../img/iconos/anadir.png" alt="+"></button>
+       
 <div id="vistaForm">
     <form id="form-end" name="formularioCentro" action="index.php?action=aniadirCentro&controller=centros" method="post">
 
         <label for="titulo">Título:</label>
-        <input type="text" id="titulo" name="titulo">
+        <input style="height: 80px" type="text" id="titulo" name="titulo">
         <span id="titulo-error" class="error-message"></span><br><br>
 
-        <label for="descripcion">Reflexion:</label>
+        <label for="descripcion">Reflexión:</label>
         <input type="text" id="descripcion" name="descripcion">
         <span id="descripcion-error" class="error-message"></span><br><br>
 
         <button id="send" type="submit">Enviar</button>
         <span id="status-message" style="margin-left: 100px;"></span>
-        <br><br>
-        
+        <button class="boton" style="float: right"><img src="../img/iconos/x.png" alt="-"></button>
     </form>
 </div>
 
