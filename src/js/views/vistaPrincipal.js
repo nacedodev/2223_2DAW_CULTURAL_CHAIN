de08fe37a96790278773bfc29fa2d6db013e60a3
Mario <mariofuentesfernandez.guadalupe@alumnado.fundacionloyola.net>
2023-12-04 18:52:45 +0100

    ajax para mostrar datos del form

--- src/js/views/vistaPrincipal.js
+++ src/js/views/vistaPrincipal.js
@@ -19,14 +19,16 @@ export class VistaPrincipal extends Vista {
 
     this.personajes
     this.cargarPersonajes()
+    this.cargarCentros()
     this.cargarNiveles()
-  
+
     const btnRestart = document.getElementById('restart')
     const btnRanking = this.base.querySelectorAll('button')[3]
     const btnSettings = this.base.querySelectorAll('button')[4]
     const btnFacil = document.getElementById('facil')
     const btnMedio = document.getElementById('medio')
     const btnDificil = document.getElementById('dificil')
+
     this.hora = document.getElementById('hora')
     this.puntuacion = document.getElementById('puntuacion')
     this.btnTheme = this.base.querySelector('#theme')
@@ -34,7 +36,10 @@ export class VistaPrincipal extends Vista {
     this.divIzq = document.getElementById('divizquierda')
     this.gameStarted = false
     this.clickerMode = false
-    const personajes = this.base.querySelectorAll('.personaje')
+    
+    this.selectCentros = document.getElementById('centro')
+    this.selectLocalidades = document.getElementById('localidad')
+
     this.divPersonajes = document.getElementById('divderecha')
     this.divImagenesPersonjanes = document.getElementById('divpersonajes')
     this.titulo = document.getElementById('titulo')
@@ -72,6 +77,10 @@ export class VistaPrincipal extends Vista {
     this.cantidadBanderas=0
     this.conflictoActual=1
 
+    //Centros
+    this.arrayCentros = []
+    this.arrayLocalidades = []
+
     this.puntuacion = document.getElementById('puntuacion')
     this.posX
     this.posY
@@ -85,11 +94,6 @@ export class VistaPrincipal extends Vista {
     this.personita
     this.tiempo
 
-    // habilitamos la capacidad de hacer drag & drop a los personajes
-    personajes.forEach(personaje => {
-      personaje.addEventListener('dragstart', this.dragStart)
-      personaje.addEventListener('dragend', this.dragEnd)
-    })
 
     // Agregar eventos de arrastrar y soltar al tablero
     this.tablero.addEventListener('dragover', this.dragOver)
@@ -351,7 +355,7 @@ export class VistaPrincipal extends Vista {
     const personajeSelected = personaje.cloneNode(true)
 
     //Establecer el nombre del nivel
-    this.nombreapp.textContent = this.niveles[this.nivelActual].nombre
+    this.nombreapp.textContent = this.niveles[this.nivelActual].nombre.toUpperCase()
 
     // Obtener las coordenadas del evento de soltar en relación con el tablero
 
@@ -432,6 +436,10 @@ export class VistaPrincipal extends Vista {
     if (event.key == 'a' && this.dir != 2) this.dir = 4
 
     // Verifica si el evento es táctil
+    if(event.touches && !this.showForm && this.partidaTerminada) {
+      this.controlador.overlayForm(this.form)
+      this.showForm = false
+    }
     if (event.touches && event.touches.length > 0) {
     // Obtén las coordenadas x del toque
       const touchX = event.touches[0].clientX
@@ -516,17 +524,22 @@ export class VistaPrincipal extends Vista {
       const tableroAncho = this.tablero.clientWidth-this.part[0].offsetWidth*2
       const tableroAlto = this.tablero.clientHeight-this.part[0].offsetHeight*2
 
+       // Obtén todos los elementos img dentro del div
+      var imagenes = this.divImagenesPersonjanes.getElementsByTagName('img');
+
+      // Convierte la colección de imágenes en un array
+      var arrayDeImagenes = Array.from(imagenes);
+
       // Crear un nuevo elemento img en lugar de div
       const nuevaImagen = document.createElement('img')
       nuevaImagen.style.width = '2.3%'
       nuevaImagen.style.animation = 'personaQuieta 3s infinite'
 
-      const numeroAleatorio = Math.floor(Math.random() * 15) + 1
-      const numeroFormateado = ('00' + numeroAleatorio).slice(-3)
+      const numeroAleatorio = Math.floor(Math.random() * arrayDeImagenes.length)
 
       // Crear la URL de la imagen utilizando el número formateado
 
-      nuevaImagen.src = 'img/personajes/person' + numeroFormateado + '.png'
+      nuevaImagen.src = arrayDeImagenes[numeroAleatorio].src
 
       // Establecer el estilo del borde de la nueva imagen
 
@@ -696,6 +709,10 @@ export class VistaPrincipal extends Vista {
             // Ahora, arrayResultado contiene la estructura deseada
             this.niveles = arrayResultado;
             this.cantidadBanderas = this.niveles[this.nivelActual].conflictos.length;
+            this.listapersonajes.forEach(personaje => {
+              personaje.addEventListener('dragstart', this.dragStart)
+              personaje.addEventListener('dragend', this.dragEnd)
+            })
         },
         error: function (status, error) {
             console.error("Error en la solicitud AJAX: " + status + " - " + error);
@@ -727,6 +744,9 @@ export class VistaPrincipal extends Vista {
             this.personajes = arrayResultado;
             this.controlador.almacenarNames(this.cargarNombres())
             this.aniadirPersonajes()
+
+            this.listapersonajes = this.base.querySelectorAll('.personaje')
+            
             this.asignarNames()
         },
         error: function (status, error) {
@@ -734,20 +754,71 @@ export class VistaPrincipal extends Vista {
         }
     });
   }
-  aniadirPersonajes() {
-    this.personajes.forEach((personaje) => {
-        var figure = document.createElement("figure");
-        var img = document.createElement("img");
-        var figcaption = document.createElement("figcaption");
-        
-        img.src = "data:image/png;base64," + personaje.imagen;
-        img.className = "personaje";
-        
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        
-        this.divImagenesPersonjanes.appendChild(figure);
+  cargarCentros() {
+    var arrayResultado = []; // Array para almacenar la información estructurada
+
+    $.ajax({
+        url: "http://localhost/2324_2DAW_CULTURAL_CHAIN/src/php/ajaxcentros.php",
+        type: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: (data) => {
+            // Iterar sobre los datos recibidos
+            for (var i = 0; i < data.length; i++) {
+                var otherData = data[i].otherData;
+
+                // Estructurar los datos
+                var elemento = {
+                    nombre: otherData.nombre,
+                    localidad: otherData.localidad,
+                };
+
+                // Agregar el elemento al arrayResultado
+                arrayResultado.push(elemento);
+
+                // Verificar si la localidad ya está en el arrayLocalidades
+                if (!this.arrayLocalidades.includes(elemento.localidad)) {
+                    // Si no está, agregarla al arrayLocalidades
+                    this.arrayLocalidades.push(elemento.localidad);
+                }
+            }
+
+            // Filtrar arrayResultado para obtener solo los centros
+            this.arrayCentros = arrayResultado.map((elemento) => elemento.nombre);
+            this.aniadirOptions()
+        },
+        error: function (status, error) {
+            console.error("Error en la solicitud AJAX: " + status + " - " + error);
+        }
     });
+}
+aniadirOptions(){
+  // Añadir opciones al select de centros
+  for (var i = 0; i < this.arrayCentros.length; i++) {
+    this.selectCentros.innerHTML += "<option>"+this.arrayCentros[i]+"</option>"
+    console.log("hola")
+  }
+
+  // Añadir opciones al select de localidades
+  for (var i = 0; i < this.arrayLocalidades.length; i++) {
+    this.selectLocalidades.innerHTML += "<option>"+this.arrayLocalidades[i]+"</option>"
+}
+}
+aniadirPersonajes() {
+  this.personajes.forEach((personaje,i) => {
+      var figure = document.createElement("figure");
+      var img = document.createElement("img");
+      var figcaption = document.createElement("figcaption");
+      
+      img.src = "data:image/png;base64," + personaje.imagen;
+      img.className = "personaje";
+      img.id = this.personajes[i].nombre
+      
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      
+      this.divImagenesPersonjanes.appendChild(figure);
+  });
 }
   cargarNombres(){
     let resultado = []
@@ -874,7 +945,7 @@ pasarnivel(){
   if(this.nivelActual===this.niveles.length-1)
     this.nivelActual=-1
   this.nivelActual++
-  this.nombreapp.textContent = this.niveles[this.nivelActual].nombre
+  this.nombreapp.textContent = this.niveles[this.nivelActual].nombre.toUpperCase()
   this.cargarFondo(this.niveles[this.nivelActual].imagen)
 
   let elementosGenerados = this.tablero.querySelectorAll('.generado');
